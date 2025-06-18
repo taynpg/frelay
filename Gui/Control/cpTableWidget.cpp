3248713cead683126d24a6f1a3cefc6e653a2999
taynpg <taynpg@163.com>
2025-06-18 10:42:53 +0800

    compare: add search and replace.

---
+++ Gui/Control/cpTableWidget.cpp
@@ -0,0 +1,21 @@
+﻿#include "cpTableWidget.h"
+
+#include <QApplication>
+#include <QDrag>
+#include <QMimeData>
+#include <QPainter>
+
+CpTableWidget::CpTableWidget(QWidget* parent) : QTableWidget(parent)
+{
+}
+
+CpTableWidget::~CpTableWidget()
+{
+}
+
+void CpTableWidget::dragEnterEvent(QDragEnterEvent* event)
+{
+
+}
+
+
---
+++ Gui/Control/cpTableWidget.h
@@ -0,0 +1,21 @@
+﻿#ifndef CP_TABLEWIDET_H
+#define CP_TABLEWIDET_H
+
+#include <QTableWidget>
+#include <QDropEvent>
+
+class CpTableWidget : public QTableWidget
+{
+    Q_OBJECT
+public:
+    explicit CpTableWidget(QWidget* parent = nullptr);
+    ~CpTableWidget() override;
+
+protected:
+    void dragEnterEvent(QDragEnterEvent* event);
+
+protected:
+    QPoint startPos_;
+};
+
+#endif // CP_TABLEWIDET_H
