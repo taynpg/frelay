a4d9a5acb23a4325540e646a6c385b95e55063ff
taynpg <taynpg@163.com>
2025-11-08 18:30:29 +0800

    com：编译设定。

---
+++ Script/qt6build-MinGW-Official.bat
@@ -0,0 +1,9 @@
+@echo on
+
+set QT_DIR=C:/Qt/6.8.3/mingw_64
+cd ..
+cmake -G "MinGW Makefiles" -Bbuild-qt6 -S. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_DIR% -DQT_DEFAULT_MAJOR_VERSION=6 -DCOMPILE_GUI=ON 
+cmake --build build-qt6 --config Release
+cd build-qt6
+cpack
+pause
