832b30c73bc9e2527feb4a8a875c3643d53a2c0e
taynpg <taynpg@163.com>
2025-11-16 21:22:07 +0800

    pack：打包包含styles文件。

--- Script/qt6build.bat
+++ Script/qt6build.bat
@@ -1,8 +1,8 @@
 @echo on
 
-set QT_DIR=C:/Qt/6.8.3
+set QT_DIR=C:/Qt/Qt6
 cd ..
-cmake -Bbuild-qt6 -S. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_DIR% -DQT_DEFAULT_MAJOR_VERSION=6 -DCOMPILE_GUI=ON -DRELEASE_MARK=ON
+cmake -Bbuild-qt6 -S. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_DIR% -DQT_DEFAULT_MAJOR_VERSION=6 -DCOMPILE_GUI=ON -DRELEASE_MARK=ON
 cmake --build build-qt6 --config Release
 cd build-qt6
 cpack
