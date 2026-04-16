d6153a2e68f4648df76264a8cf47d3cdcdf3e77d
taynpg <taynpg@163.com>
2026-04-16 16:52:16 +0800

    添加Mingw64相关

--- Script/qt6-MinGW-Dev.bat
+++ Script/qt6-MinGW-Dev.bat
@@ -1,6 +1,6 @@
 @echo on
 
-set QT_DIR=C:/local/qt6
+set QT_DIR=C:/mingw/qt6
 cd ..
 cmake -G "MinGW Makefiles" -Bbuild-qt6-dev -S. -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_DIR% -DQT_DEFAULT_MAJOR_VERSION=6 -DCOMPILE_GUI=ON -DQT_ROOT_PATH=%QT_DIR%
 cmake --build build-qt6-dev --config Release
