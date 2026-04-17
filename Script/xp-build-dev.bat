68431232cedfad7b959ab2cb4d4176148907d835
taynpg <taynpg@163.com>
2026-04-17 14:17:19 +0800

    xp dev脚本

---
+++ Script/xp-build-dev.bat
@@ -0,0 +1,12 @@
+@echo on
+
+set QT_DIR=C:/Qt/Qt5.7.1/5.7/mingw53_32
+set COMPILE_DIR=C:/Qt/Qt5.7.1/Tools/mingw530_32
+set PATH=%COMPILE_DIR%/bin;%PATH%
+
+cd ..
+cmake -Bbuild-xp -S. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=%QT_DIR% -DQT_DEFAULT_MAJOR_VERSION=5 -DXP_PLATFORM_SUPPORT=ON -DCOMPILE_GUI=ON -DQT_ROOT_PATH=%QT_DIR%
+cmake --build build-xp --config Release
+cd build-xp
+cpack
+pause
